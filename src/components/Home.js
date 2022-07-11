9f4213d4e4309eab4f98d0110240bcd378b21bef
amankumar2509 <aman.kumar1_cs19@gla.ac.in>
2022-07-11 12:43:03 +0530

    moviesapp

--- src/components/Home.js
+++ src/components/Home.js
@@ -26,7 +26,7 @@ const Home = () => {
   };
 
   const getMovieRequest = async (searchValue) => {
-    const url = `http://www.omdbapi.com/?s=${searchValue}&apikey=263d22d8`;
+    const url = `https://www.omdbapi.com/?s=${searchValue}&apikey=263d22d8`;
 
     const response = await fetch(url);
     const responseJson = await response.json();
